2e490d644ad6a6669f7f3433b6e2800d301ca3b5
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-07-14 20:37:01 -0300

    create class api to request youtube api, create class video to map when request done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertube/home.dart';
+import 'package:fluttertube/screens/api.dart';
+import 'package:fluttertube/screens/home.dart';
 
 void main() {
+
+  Api api = Api();
+  api.search("wow");
+
   runApp(MyApp());
 }
 
