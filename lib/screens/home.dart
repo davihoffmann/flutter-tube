af4cd62477b309625835e9843f4fff0a763796c8
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-07-14 20:44:38 -0300

    create home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Ola'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("images/yt_logo_rgb_dark.png"),
+        ),
+        elevation: 1,
+        backgroundColor: Colors.black87,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(icon: Icon(Icons.star), onPressed: () => {}),
+          IconButton(icon: Icon(Icons.search), onPressed: () => {})
+        ],
+      ),
+      body: Container(),
     );
   }
 }
